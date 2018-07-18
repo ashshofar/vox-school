54e8c27241d59d56764a754432197fed139a810b
ikhsan <shofar.ikhsan@gmail.com>
2018-07-18 16:20:39 +0700

    temp testing

---
+++ tests/Feature/SchoolTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class SchoolTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $appURL = env('APP_URL');
+
+        $urls = [
+            '/',
+            '/school',
+            '/school/create',
+            '/student/create'
+        ];
+
+        echo  PHP_EOL;
+
+        foreach ($urls as $url) {
+            $response = $this->get($url);
+            if((int)$response->status() !== 200){
+                echo  $appURL . $url . ' (FAILED) did not return a 200.';
+                $this->assertTrue(false);
+            } else {
+                echo $appURL . $url . ' (success ?)';
+                $this->assertTrue(true);
+            }
+            echo  PHP_EOL;
+        }
+
+    }
+}
