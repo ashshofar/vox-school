bbddfc7601bfdf645b836ba1ea1a88122d37129b
ikhsan <shofar.ikhsan@gmail.com>
2018-07-18 16:29:29 +0700

    fix route /

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'SchoolController@index')->name('school.index');
 
 
 // Route::group(['prefix' => 'organizer'], function() {
